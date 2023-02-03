217e3b4bdceb7672cf6bc64f0d7df95228dc046d
Kerim <mehmetkerimozek@gmail.com>
2023-02-03 12:59:30 +0300

    added mainScreen picker

---
+++ SwiftUI-Movieva/MainScreen/Picker/MainPicker.swift
@@ -0,0 +1,39 @@
+//
+//  MainPicker.swift
+//  SwiftUI-Movieva
+//
+//  Created by PSI_ErsanV on 3.02.2023.
+//
+
+import SwiftUI
+
+struct MainPicker: View {
+    
+    var views = ["Latest", "Upcoming", "TopRated"]
+        @State private var selectedView = 0
+    
+    var body: some View {
+        VStack {
+                   Picker(selection: $selectedView, label: Text("Select a view")) {
+                       ForEach(0..<views.count, id: \.self) {
+                           Text(self.views[$0]).tag($0)
+                       }
+                   }
+                   .pickerStyle(SegmentedPickerStyle())
+
+                   if selectedView == 0 {
+                       LatestView()
+                   } else if selectedView == 1 {
+                       UpcomingView()
+                   } else {
+                       TopRatedView()
+                   }
+               }
+    }
+}
+
+struct MainPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPicker()
+    }
+}
