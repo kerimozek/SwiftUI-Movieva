b7a141a3faa09aa970428a3ea946d5cb0869e74a
Kerim <mehmetkerimozek@gmail.com>
2023-02-03 12:58:32 +0300

    refactor contentView

--- SwiftUI-Movieva/ContentView.swift
+++ SwiftUI-Movieva/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         
         TabView{
-            MainScreen()
+            TopSlider()
                 .tabItem {
                     Image(systemName: "house")
                 }
