8615d49e0dae2b08d7938c6bba3e4923ce007af0
Kerim <mehmetkerimozek@gmail.com>
2023-02-03 12:59:49 +0300

    refactor topSlider

---
+++ SwiftUI-Movieva/MainScreen/TopSlider/View/TopSlider.swift
@@ -0,0 +1,37 @@
+//
+//  MainScreen.swift
+//  SwiftUI-API-Call-Decode-JSON
+//
+//  Created by PSI_ErsanV on 2.02.2023.
+//
+
+import SwiftUI
+
+struct TopSlider: View {
+    
+    let images = ["mikasa", "ereh", "levi", "mikasa", "ereh", "levi"]
+    let colors = [Color.red, Color.green, Color.blue, Color.orange, Color.pink]
+    
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center) {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200)
+                }
+            }
+            
+        }
+        .padding(.horizontal, 12)
+    }
+}
+
+
+struct TopSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        TopSlider()
+    }
+}
