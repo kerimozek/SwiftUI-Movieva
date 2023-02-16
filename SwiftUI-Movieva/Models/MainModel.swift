1a5a3492ed1a02307c79e610f140ad141f4b1741
Kerim <mehmetkerimozek@gmail.com>
2023-02-16 12:06:25 +0300

    refactor mainModel

--- SwiftUI-Movieva/Models/MainModel.swift
+++ SwiftUI-Movieva/Models/MainModel.swift
@@ -31,23 +31,54 @@ struct Company: Codable {
 }
 
 
+// MARK: - Movie
 struct Movie: Codable {
     let page: Int?
     let results: [ResultMovie]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
 }
 
+// MARK: - Result
 struct ResultMovie: Codable {
-    var adult: Bool?
-    var backdrop_path: String?
-    var genre_ids: [Int]?
-    var id: Int?
-    var original_language, original_title, overview: String?
-    var popularity: Double?
-    var poster_path: String?
-    var release_date, title: String?
-    var video: Bool?
-    var vote_average: Double?
-    var vote_count: Int?
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage: OriginalLanguage?
+    let originalTitle, overview: String?
+    let popularity: Double?
+    let posterPath, releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case es = "es"
+    case ko = "ko"
+    case no = "no"
 }
 
 
