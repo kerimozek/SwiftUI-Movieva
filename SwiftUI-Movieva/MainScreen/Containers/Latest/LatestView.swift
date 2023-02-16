3e30a558d01cafde1098a313450e155a08d6444d
Kerim <mehmetkerimozek@gmail.com>
2023-02-16 12:05:35 +0300

    refactor latestView

--- SwiftUI-Movieva/MainScreen/Containers/Latest/LatestView.swift
+++ SwiftUI-Movieva/MainScreen/Containers/Latest/LatestView.swift
@@ -1,86 +1,47 @@
-//
-//  LatestView.swift
-//  SwiftUI-Movieva
-//
-//  Created by PSI_ErsanV on 3.02.2023.
-//
-
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct LatestView: View {
-
-    @State private var imageData: Data?
     
-
-    let movies: Movie
-
-    //    let images = ["mikasa", "ereh", "levi", "mikasa", "ereh", "levi"]
-
-
+    @ObservedObject private var vm = MovieViewModel()
+    
     var body: some View {
-
+        
         VStack {
-            if let data = imageData {
-                Image(uiImage: UIImage(data: data)!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200)
-                    .cornerRadius(12)
-            } else {
-                Text("Loading image...")
-            }
-        }
-        .onAppear {
-            self.loadImage()
-        }
-    }
-
-    private func loadImage() {
-        guard let url = URL(string: "https://image.tmdb.org/t/p/w200\(String(describing: movies.results?.first?.poster_path))") else {
-            return
-        }
-
-        DispatchQueue.global().async {
-            if let data = try? Data(contentsOf: url) {
-                DispatchQueue.main.async {
-                    self.imageData = data
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(vm.movies ?? vm.placeholders, id: \.id) { item in
+                        LatestSingleMovie(item: item)
+                    }
                 }
             }
         }
+        .onAppear {
+            vm.fetchMovies()
+        }
     }
 }
+
+
+struct LatestSingleMovie: View {
+    
+    var item: ResultMovie
+    
+    var body: some View {
+        VStack {
+            WebImage(url: URL(string: "https://image.tmdb.org/t/p/w500\(item.posterPath ?? "")"))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200)
+                .redacted(reason: item.posterPath == nil ? .placeholder : .init())
+                .cornerRadius(12)
+        }
+    }
+}
+
 
 struct LatestView_Previews: PreviewProvider {
     static var previews: some View {
-        LatestView(movies: Movie(page: 1, results: [ResultMovie(adult: false, backdrop_path: "path", genre_ids: [1, 2], id: 1, original_language: "en", original_title: "Title", overview: "Overview", popularity: 10.0, poster_path: "pFlaoHTZeyNkG83vxsAJiGzfSsa", release_date: "2022-02-14", title: "Title", video: false, vote_average: 8.0, vote_count: 100)]))
+        LatestView()
     }
 }
-
-
-
-
-
-//    var body: some View {
-//        GeometryReader { geometry in
-//                    ScrollView {
-//                        VStack(spacing: 20) {
-//                            ForEach(0..<6) { index in
-//                                HStack(spacing: 20) {
-//                                    ForEach(0..<3) { columnIndex in
-//                                        Image("levi")
-//                                            .resizable()
-//                                            .aspectRatio(contentMode: .fill)
-//                                            .frame(height: 150)
-//                                            .padding(6)
-//                                            .background(Color.red)
-//                                            .cornerRadius(8)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                        .padding()
-//                    }
-//                    .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
-//                }
-//            }
-//        }
