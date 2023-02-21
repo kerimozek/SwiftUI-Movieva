423a26777dbc34c2d4509d27a2a1873c59bacaed
Kerim <mehmetkerimozek@gmail.com>
2023-02-21 10:31:01 +0300

    refactor detailScreen

--- SwiftUI-Movieva/DetailScreen/DetailScreen.swift
+++ SwiftUI-Movieva/DetailScreen/DetailScreen.swift
@@ -92,7 +92,7 @@ struct DetailScreen_Previews: PreviewProvider {
                                        backdropPath: "/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg",
                                        genreIDS: [28,12,878],
                                        id: 505642,
-                                       originalLanguage: .en,
+                                       originalLanguage: "en",
                                        originalTitle: "Black Panther: Wakanda Forever",
                                        overview: "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
                                        popularity: 4646.778,
