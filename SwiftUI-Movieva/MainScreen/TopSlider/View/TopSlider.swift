4ccd14ed68efed5baeb5a33a1ba5ad55df25f563
Kerim <mehmetkerimozek@gmail.com>
2023-02-21 10:47:02 +0300

    refactor topSlider

--- SwiftUI-Movieva/MainScreen/TopSlider/View/TopSlider.swift
+++ SwiftUI-Movieva/MainScreen/TopSlider/View/TopSlider.swift
@@ -8,32 +8,6 @@
 import SwiftUI
 import SDWebImageSwiftUI
 
-struct TopSlider: View {
-    
-    @ObservedObject private var vm = MovieViewModel()
-    
-    var body: some View {
-        
-        NavigationView {
-            VStack {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 12) {
-                        ForEach(vm.movies ?? vm.placeholders, id: \.id) { item in
-                            NavigationLink(
-                                destination: DetailScreen(item: item),
-                                label: {
-                                    TopSliderSingle(item: item)
-                                })
-                        }
-                    }
-                }
-            }
-            .onAppear {
-                vm.fetchMovies()
-            }
-        }
-    }
-}
 
 
 struct TopSliderSingle: View {
@@ -53,8 +27,21 @@ struct TopSliderSingle: View {
 }
 
 
-struct TopSlider_Previews: PreviewProvider {
+struct TopSliderSingle_Previews: PreviewProvider {
     static var previews: some View {
-        TopSlider()
+        TopSliderSingle(item: ResultMovie(adult: true,
+                                                 backdropPath: "/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg",
+                                                 genreIDS: [28,12,878],
+                                                 id: 505642,
+                                                 originalLanguage: "en",
+                                                 originalTitle: "Black Panther: Wakanda Forever",
+                                                 overview: "Queen Ramonda, Shuri, M’Baku, Okoye and the Dora Milaje fight to protect their nation from intervening world powers in the wake of King T’Challa’s death.  As the Wakandans strive to embrace their next chapter, the heroes must band together with the help of War Dog Nakia and Everett Ross and forge a new path for the kingdom of Wakanda.",
+                                                 popularity: 4646.778,
+                                                 posterPath: "/sv1xJUazXeYqALzczSZ3O6nkH75.jpg",
+                                                 releaseDate: "2022-11-09",
+                                                 title: "Black Panther: Wakanda Forever",
+                                                 video: false,
+                                                 voteAverage: 7.5,
+                                                 voteCount: 3132))
     }
 }
