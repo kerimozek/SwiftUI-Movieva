af5d3cdf28078a4f5a83ae0675531f9d5abb0427
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-05-09 20:45:48 +0300

    refactor latestView

--- SwiftUI-Movieva/MainScreen/Containers/Latest/LatestView.swift
+++ SwiftUI-Movieva/MainScreen/Containers/Latest/LatestView.swift
@@ -9,27 +9,46 @@
 import SwiftUI
 
 struct LatestView: View {
-    
     @StateObject private var vm = MovieViewModel()
     
     var body: some View {
-        
         ScrollView(.vertical, showsIndicators: false) {
             LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 12) {
-                ForEach(vm.movies ?? vm.placeholders, id: \.id) { item in
+                ForEach(vm.movies.indices, id: \.self) { index in
+                    let item = vm.movies[index]
                     NavigationLink(
                         destination: DetailScreen(item: item),
                         label: {
                             LatestSingleMovie(item: item)
                                 .id(item.originalTitle) // provide a unique identifier
                         })
+                    
+                    if index == vm.movies.count - 1 {
+                        ProgressView()
+                            .onAppear {
+                                if !vm.fetchNextPage {
+                                    vm.fetchNextPage = true
+                                    vm.fetchMovies(shouldLoadNextPage: true)
+                                }
+                            }
+                            .onDisappear {
+                                vm.fetchNextPage = false
+                            }
+                    }
                 }
             }
             .padding(.horizontal)
-            .onAppear(perform: vm.fetchMovies)
+        }
+        .onAppear {
+            vm.resetMovies()
+            vm.fetchMovies()
         }
     }
 }
+
+
+
+
 
 
 struct LatestView_Previews: PreviewProvider {
