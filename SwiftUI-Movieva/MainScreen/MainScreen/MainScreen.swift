38054849894c1c6b020c820a077b6ca0ea238cad
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-05-09 20:44:56 +0300

    refactor mainScreen

--- SwiftUI-Movieva/MainScreen/MainScreen/MainScreen.swift
+++ SwiftUI-Movieva/MainScreen/MainScreen/MainScreen.swift
@@ -6,11 +6,11 @@ struct MainScreen: View {
     
     var body: some View {
         
-        NavigationView{
+        NavigationView {
             ScrollView {
-                ZStack{
-                    VStack(){
-                        Section{
+                ZStack {
+                    VStack {
+                        Section {
                             Text("What do you want to watch")
                                 .font(.title2)
                                 .foregroundColor(.black)
@@ -19,42 +19,64 @@ struct MainScreen: View {
                         }
                         .padding(16)
                         
-                        Section{
+                        Section {
                             ScrollView(.horizontal, showsIndicators: false) {
                                 HStack(spacing: 12) {
-                                    ForEach(vm.movies ?? vm.placeholders, id: \.id) { item in
+                                    ForEach(vm.movies.indices, id: \.self) { index in
+                                        let item = vm.movies[index]
                                         NavigationLink(
                                             destination: DetailScreen(item: item),
                                             label: {
                                                 TopSliderSingle(item: item)
                                             })
+                                        
+                                        if index == vm.movies.count - 1 {
+                                            ProgressView()
+                                                .onAppear {
+                                                    if !vm.fetchNextPage {
+                                                        vm.fetchNextPage = true
+                                                        vm.nextPage()
+                                                    }
+                                                }
+                                                .onDisappear {
+                                                    vm.fetchNextPage = false
+                                                }
+                                        }
                                     }
                                 }
                             }
-                        }
-                        .padding(.horizontal, 16)
-                        .padding(.bottom, 8)
-                        
-                        VStack{
-                            Section{
-                                MainPicker()
+                            .padding(.horizontal, 16)
+                            .padding(.bottom, 8)
+                            .onDisappear {
+                                vm.resetMovies()
                             }
-                            .padding(8)
+                            
+                            VStack {
+                                Section {
+                                    MainPicker()
+                                }
+                                .padding(8)
+                            }
+                            .frame(height: 600)
+                            .padding(.bottom, 8)
                         }
-                        .frame(height: 600)
-                        .padding(.bottom, 8)
+                        .padding(.bottom, 20)
                     }
-                    .padding(.bottom, 20)
+                    .onAppear {
+                        vm.fetchMovies()
+                    }
+                    .onDisappear {
+                        vm.resetMovies()
+                    }
                 }
-                .onAppear(perform: vm.fetchMovies)
             }
         }
     }
+    
 }
-
-
-struct MainScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        MainScreen()
+    
+    struct MainScreen_Previews: PreviewProvider {
+        static var previews: some View {
+            MainScreen()
+        }
     }
-}
