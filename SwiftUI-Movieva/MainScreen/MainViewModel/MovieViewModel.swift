1773b72983b4c10400c481799044afc66306962a
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2023-05-09 20:45:21 +0300

    refactor MovieViewModel

--- SwiftUI-Movieva/MainScreen/MainViewModel/MovieViewModel.swift
+++ SwiftUI-Movieva/MainScreen/MainViewModel/MovieViewModel.swift
@@ -8,62 +8,45 @@
 import SwiftUI
 
 class MovieViewModel: ObservableObject {
-    @Published var movies: [ResultMovie]?
-    
-    public var placeholders = Array(repeating: ResultMovie(adult: nil, backdropPath: nil, genreIDS: nil, id: 1, originalLanguage: nil, originalTitle: nil, overview: nil, popularity: nil, posterPath: nil, releaseDate: nil, title: nil, video: nil, voteAverage: nil, voteCount: nil), count: 10)
-    
-    func fetchMovies() {
-        let usersUrlString = "https://api.themoviedb.org/3/movie/popular?api_key=fd24fe5ba58021d3f54a2a7c04297951&language=en-US"
-        let url = URL(string: usersUrlString)
-        
-        var request = URLRequest(url: url!)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
-            if let error = error {
-                print("error: \(error.localizedDescription)")
-            } else {
-                guard let data = data else { return }
-                
-                do {
-                    let result = try JSONDecoder().decode(Movie.self, from: data)
-                    DispatchQueue.main.async {
-                        self?.movies = result.results
+    @Published var movies: [ResultMovie] = []
+    @Published var fetchNextPage = false
+    private var currentPage = 1
+    private var isFetching = false
+
+    func fetchMovies(shouldLoadNextPage: Bool = false) {
+        guard !isFetching else { return }
+        isFetching = true
+        let urlString = APIURLs.popularMovies(page: currentPage)
+        guard let url = URL(string: urlString) else { return }
+
+        NetworkManager.shared.request(type: Movie.self, url: url, method: .get) { [weak self] result in
+            switch result {
+            case .success(let movie):
+                DispatchQueue.main.async {
+                    if shouldLoadNextPage {
+                        self?.movies.append(contentsOf: movie.results ?? [])
+                    } else {
+                        self?.movies = movie.results ?? []
                     }
-                } catch {
-                    print("error: \(error.localizedDescription)")
+                    self?.currentPage += 1
+                    self?.isFetching = false
                 }
+            case .failure(let error):
+                print("Error: \(error.localizedDescription)")
+                self?.isFetching = false
             }
-        }.resume()
+        }
     }
     
-    func fetchUpcomingMovies() {
-        let usersUrlString = "https://api.themoviedb.org/3/movie/upcoming?api_key=fd24fe5ba58021d3f54a2a7c04297951&language=en-US"
-        let url = URL(string: usersUrlString)
-        
-        var request = URLRequest(url: url!)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
-            if let error = error {
-                print("error: \(error.localizedDescription)")
-            } else {
-                guard let data = data else { return }
-                
-                do {
-                    let result = try JSONDecoder().decode(Movie.self, from: data)
-                    DispatchQueue.main.async {
-                        self?.movies = result.results
-                    }
-                } catch {
-                    print("error: \(error.localizedDescription)")
-                }
-            }
-        }.resume()
+    func nextPage() {
+        fetchMovies(shouldLoadNextPage: true)
     }
     
-    
-    
+    func resetMovies() {
+        movies = []
+        currentPage = 1
+    }
 }
+
 
 
