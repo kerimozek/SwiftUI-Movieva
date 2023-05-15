0c4068a5962e38709c1af8aa162a3b9ecb399a9b
Kerim <mehmetkerimozek@gmail.com>
2023-05-15 12:37:42 +0300

    refactor SearchScreen

--- SwiftUI-Movieva/SearchScreen/SearchScreen.swift
+++ SwiftUI-Movieva/SearchScreen/SearchScreen.swift
@@ -8,16 +8,52 @@
 import SwiftUI
 
 struct SearchScreen: View {
+    @StateObject private var vm = MovieViewModel()
+    
     var body: some View {
-        Text("Search Screen")
-            .bold()
-            .fontWeight(.black)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color(.systemBlue))
-            .cornerRadius(10)
+        VStack{
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 12) {
+                    ForEach(vm.movies.indices, id: \.self) { index in
+                        let item = vm.movies[index]
+                        NavigationLink(
+                            destination: DetailScreen(item: item),
+                            label: {
+                                LatestSingleMovie(item: item)
+                                    .id(item.originalTitle) // provide a unique identifier
+                            })
+                        
+                        if index == vm.movies.count - 1 {
+                            ProgressView()
+                                .onAppear {
+                                    if !vm.fetchNextPage {
+                                        vm.fetchNextPage = true
+                                        vm.fetchMovies(shouldLoadNextPage: true)
+                                    }
+                                }
+                                .onDisappear {
+                                    vm.fetchNextPage = false
+                                }
+                        }
+                    }
+                }
+                .padding(16)
+            }
+            VStack{
+                Text("HHHHHH")
+            }
+            .padding(.top, 200)
+        }
+        
+        .onAppear {
+            vm.fetchMovies()
+        }
+        .onDisappear {
+            vm.resetMovies()
+        }
     }
 }
+
 
 struct SearchScreen_Previews: PreviewProvider {
     static var previews: some View {
